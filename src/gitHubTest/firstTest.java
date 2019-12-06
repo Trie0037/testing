c5faee65cd68ae3c98ffae4ebcb9e31aed02175b
Trie0037 <trie0037@gmail.com>
2019-12-05 18:28:15 -0600

    test1

--- src/gitHubTest/firstTest.java
+++ src/gitHubTest/firstTest.java
@@ -1,10 +1,10 @@
-package selenium;
+package gitHubTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Selenium {
+public class firstTest {
 	@Test
 	private void test() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
